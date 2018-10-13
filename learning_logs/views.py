c10a0d68de9678106bc02ee1b35d14587c059bc5
sunyongdi <914767648@qq.com>
2018-10-13 16:43:07 +0800

    Started project .

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse # Django2.0更改了core.urlresolvers
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, Http404
 
 # Create your views here.
 def index(request):
@@ -14,7 +15,7 @@ def index(request):
 @login_required
 def topics(request):
 	"""显示所有的主题"""
-	topics = Topic.objects.order_by('date_added')
+	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 	context = {'topics':topics}
 	return render(request, 'learning_logs/topics.html', context)
 
@@ -22,6 +23,10 @@ def topics(request):
 def topic(request, topic_id):
 	"""显示单个主题及所有的条目"""
 	topic = Topic.objects.get(id=topic_id)
+	# 确认请求的主题属于当前用户、
+	if topic.owner != request.user:
+		raise Http404
+
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic':topic, 'entries':entries}
 	return render(request, 'learning_logs/topic.html', context)
@@ -36,7 +41,9 @@ def new_topic(request):
 		# POST提交的诗句，对数据进行处理
 		form = TopicForm(request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic = form.save(commit=False)
+			new_topic.owner = request.user
+			new_topic.save()
 			return HttpResponseRedirect(reverse('learning_logs:topics'))
 	context = {'form':form}
 	return render(request, 'learning_logs/new_topic.html', context)
@@ -64,6 +71,8 @@ def edit_entry(request, entry_id):
 	"""编辑既有条目"""
 	entry = Entry.objects.get(id=entry_id)
 	topic = entry.topic
+	if topic.owner != request.user:
+		raise Http404
 	if request.method != 'POST':
 		# 初次请求，使用当前条目进行处理
 		form = EntryForm(instance=entry)
